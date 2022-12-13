f8b10a0dfb3a72cfd9f197091eecff0eced95412
Chals <csanchezbasket@gmail.com>
2022-12-13 19:18:13 +0100

    Commit proyecto funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UD35-EJ1';
+  nombre = '';
+  cif = '';
+  direccion = '';
+  grupo = '';
+  clientes:any = [];
+
+  anadirCliente(){
+
+    let cliente = {
+      nombre:this.nombre, 
+      cif:this.cif, 
+      direccion:this.direccion, 
+      grupo:this.grupo
+    };
+    
+    this.clientes.push(cliente);
+  }
 }
--- src/app/listado-clientes/listado-clientes.component.ts
+++ src/app/listado-clientes/listado-clientes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-listado-clientes',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ListadoClientesComponent {
 
+  @Input() datosCliente: any;
+
+  constructor() {}
+
+  //ngOnInit(): void {
+
+  //}
 }
